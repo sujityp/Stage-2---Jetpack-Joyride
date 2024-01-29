694d61c474e9be00b615b3763f8a5aee78c7b75f
sujityp <sujityp@gmail.com>
2024-01-29 18:18:10 -0500

    Machine gun
    - gun sounds
    - bullet motion
    - bullet collision physics
    - bullet queue_free

--- Scripts/bird.gd
+++ Scripts/bird.gd
@@ -3,11 +3,12 @@ extends CharacterBody2D
 
 @export var JUMP_VELOCITY = 300.0
 @export var gravity: float = ProjectSettings.get_setting("physics/2d/default_gravity")
-#@export var alive = false
+@export var alive = false
 @export var vel_rotation_ratio = 0.1
 
 @onready var animPlayer = $AnimationPlayer
-@onready var flapSound = $FlapSound
+@onready var loopGunSFX = $LoopingGunSFX
+@onready var endGunSFX = $EndGunSFX
 
 var Bullets = preload("res://Scenes/bullet.tscn")
 
@@ -22,25 +23,15 @@ func _physics_process(delta):
 	if Input.is_action_pressed("fly"):# and alive:
 		velocity.y = -JUMP_VELOCITY
 		animPlayer.play("flap")
-		if not flapSound.is_playing():
-			flapSound.play(0.05)
-		
+		endGunSFX.stop()
+		if not loopGunSFX.is_playing():
+			loopGunSFX.play()
 		var bullet = Bullets.instantiate()
 		bullet.global_position = global_position
 		get_parent().add_child(bullet)
+	elif Input.is_action_just_released("fly"):
+		loopGunSFX.stop()
+		endGunSFX.play()
 	
 	rotation_degrees = vel_rotation_ratio * velocity.y
 	move_and_slide()
-#	check_alive_state()
-
-
-#func check_alive_state():
-#	if is_on_ceiling() or is_on_floor() or is_on_wall():
-#		emit_signal("dead")
-#		alive = false
-
-
-#func reset():
-#	alive = true
-#	position = Vector2(72, 230)
-#	velocity = Vector2(0, -JUMP_VELOCITY)
--- Scripts/bullet.gd
+++ Scripts/bullet.gd
@@ -6,6 +6,8 @@ extends CharacterBody2D
 @export var bounce_speed = 200
 @export var gravity = 50
 
+var rotate_towards = 0
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	velocity = Vector2(randi_range(-150,150), bullet_speed)
@@ -14,11 +16,14 @@ func _ready():
 
 func _physics_process(delta):
 	velocity.y += gravity
+	rotation = move_toward(rotation, rotate_towards, 0.3)
 	if move_and_slide():
-		bullet_speed = -0.4* bullet_speed
+		bullet_speed = -randf_range(0.3,0.5) * bullet_speed
 		velocity = Vector2(velocity.x - 200, bullet_speed)
+		rotate_towards = randi_range(-60,60)
 		$Bullet2.show()
 		$Bullet1.hide()
+		
 
 
 func _on_visible_on_screen_notifier_2d_screen_exited():
